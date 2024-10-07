92290718f6e1eb9a07cde17782c377426192bf05
kamesh Rao <kamesh122@gmail.com>
2024-10-07 14:26:26 -0700

    initial Commit

---
+++ script1.js
@@ -0,0 +1,7 @@
+let students = [
+    {name: 'John', username: 'john', pswd: '1111', gender:'Male', marks: [50, 67, 74, 32, 89], role: 'admin'},
+    {name: 'Steve', username: 'steve', pswd: '2222', gender:'Male', marks: [28, 39, 32, 22, 79], role: 'student'},
+    {name: 'Merry', username: 'merry', pswd: '3333', gender:'Female', marks: [78, 68, 43, 88, 32], role: 'student'},
+    {name: 'Mike', username: 'mike', pswd: '4444', gender:'Male', marks: [44, 65, 34, 64, 54], role: 'student'},
+    {name: 'Sarah', username: 'sarah', pswd: '5555', gender:'Female', marks: [34, 45, 23, 77, 28], role: 'student'}
+];
